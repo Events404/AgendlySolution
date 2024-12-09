ebf888bb56d47e993eb4f28cb5a4f328e53a728e
Events404 <assemkhashaba80@gmail.com>
2024-12-09 23:01:26 +0200

    Home models

---
+++ DataAccess/Repository/HomeRepository.cs
@@ -0,0 +1,21 @@
+﻿using Agendly.Data;
+using DataAccess.Repository.IRepository;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class HomeRepository : Repository<Home>, IHomeRepository
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public HomeRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+    }
+}
---
+++ Models/Home.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Home
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(300)]
+
+        public string WelcomeText { get; set; }
+        [Required]
+        [StringLength(300)]
+
+        public string AboutAgendly { get; set; }
+        [Required]
+        [StringLength(300)]
+
+        public string TaskManagementDescription { get; set; }
+        [Required]
+        [StringLength(300)]
+
+        public string TeamCollaborationDescription { get; set; }
+        [Required]
+        [StringLength(300)]
+
+        public string PerformanceInsightsDescription { get; set; }
+
+        public List<FAQ> FAQs { get; set; }
+
+    }
+}  
